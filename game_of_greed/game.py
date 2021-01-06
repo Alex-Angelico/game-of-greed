944862a40faa246c3a3bb8397eecd63f11191339
PyDrummer <anthonybeavermusic@gmail.com>
2021-01-05 21:32:59 -0800

    2_tests_pass

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -1,4 +1,4 @@
-
+import time
 from game_of_greed.game_logic import GameLogic
 from game_of_greed.banker import Banker
 
@@ -18,7 +18,8 @@ class Game:
                 Defaults to None.
         """
 
-        self.round_num = 0
+        # self.round_num = 0 change this back later
+        self.round_num = 1
 
         self._roller = roller or GameLogic.roll_dice
 
@@ -38,7 +39,24 @@ class Game:
 
     def start_game(self):
         # TODO: um, the game
+        while self.round_num < 21:
+            if not self.banker.shelved:
+                rolled_die = self._roller(6)
+                print(f"Starting round {self.round_num}")
+                print(f"Rolling 6 dice...")
+                time.sleep(1)
+                print(f"*** {rolled_die[0]} {rolled_die[1]} {rolled_die[2]} {rolled_die[3]} {rolled_die[4]} {rolled_die[5]} ***")
+                print("Enter dice to keep, or (q)uit:")
+                dice_or_quit = input("> ")
+                if dice_or_quit == 'q':
+                    self.quit_game()
+                    break
+
+        # self.round_num += 1 add this after bank/farkle
         pass
+
+    def quit_game(self):
+        print(f'Thanks for playing. You earned {self.banker.balance} points')
 
 if __name__ == "__main__":
     game = Game()
