89861a30bedc72150157590b3c80a0609ad63f56
PyDrummer <anthonybeavermusic@gmail.com>
2021-01-06 19:28:07 -0800

    basic score keeping

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -1,6 +1,6 @@
 import time
-from game_of_greed.game_logic import GameLogic
-from game_of_greed.banker import Banker
+from game_logic import GameLogic
+from banker import Banker
 
 
 class Game:
@@ -22,6 +22,7 @@ class Game:
         self.round_num = 1
 
         self._roller = roller or GameLogic.roll_dice
+        self._calculate = GameLogic.calculate_score
 
         print("Welcome to Game of Greed")
 
@@ -39,18 +40,48 @@ class Game:
 
     def start_game(self):
         # TODO: um, the game
+        num_die_to_roll = 6
         while self.round_num < 21:
+            dice_counter = 6
             if not self.banker.shelved:
-                rolled_die = self._roller(6)
+                rolled_die = self._roller(dice_counter)
                 print(f"Starting round {self.round_num}")
-                print(f"Rolling 6 dice...")
+                print(f"Rolling {dice_counter} dice...")
                 time.sleep(1)
                 print(f"*** {rolled_die[0]} {rolled_die[1]} {rolled_die[2]} {rolled_die[3]} {rolled_die[4]} {rolled_die[5]} ***")
                 print("Enter dice to keep, or (q)uit:")
                 dice_or_quit = input("> ")
+                # print(type(dice_or_quit)) It's a string currently
                 if dice_or_quit == 'q':
                     self.quit_game()
                     break
+                else:
+                    int_die = []
+                    list_die = list(dice_or_quit)
+                    for nums in list_die:
+                        int_die.append(int(nums))
+                    print(int_die) # this is now a list of ints
+                    shelf_points = self._calculate(int_die)
+                    to_subtract = len(int_die)
+                    num_die_to_roll = dice_counter - to_subtract
+                    print(f"You have {shelf_points} unbanked points and {num_die_to_roll} dice remaining")
+                    print(f"(r)oll again, (b)ank your points or (q)uit:")
+                    continue
+                    ############################
+                    # verification stretch goal:
+                    # for die in rolled_die:
+                    #     print("rolled_die:")
+                    #     print(die)
+                    #     for each in list_die:
+                    #         print("list_die:")
+                    #         print(each)
+                    # if int_die == die:
+                    #     print('True')
+                    #     break
+                    # else:
+                    #     print('False')
+                    #     break
+                    ############################
 
         # self.round_num += 1 add this after bank/farkle
         pass
--- tests/test_sim_basic.py
+++ tests/test_sim_basic.py
@@ -4,19 +4,19 @@ from game_of_greed.game import Game
 
 #pytestmark = [pytest.mark.version_2, pytest.mark.version_3]
 
-#@pytest.mark.skip("pending")
+@pytest.mark.skip("pending")
 def test_quitter():
     game = Game()
     errors = diff(game.play, path="tests/assets/quitter.sim.txt")
     assert not errors, errors[0]
 
-#@pytest.mark.skip("pending")
+@pytest.mark.skip("pending")
 def test_one_and_done():
     game = Game()
     errors = diff(game.play, path="tests/assets/one_and_done.sim.txt")
     assert not errors, errors[0]
 
-@pytest.mark.skip("pending")
+#@pytest.mark.skip("pending")
 def test_single_bank():
     game = Game()
     errors = diff(game.play, path="tests/assets/bank_one_roll_then_quit.sim.txt")
